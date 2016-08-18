c8e3370291ef9712faea6c1edd0f353536a01151
Hydraaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa <james.dasilva1111@gmail.com>
2016-08-18 14:21:00 +1000

    Adjusted Bullet script for different weapon types
    + Added enumerator for different weapon types
    + Added initialize function to set up a given weapon type
    ~ Changed Update and Collision functions based on weapon type

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,32 +1,81 @@
 ﻿using UnityEngine;
-using System.Collections;
 
-public class Bullet : MonoBehaviour {
+public class Bullet : MonoBehaviour
+{
+    enum bulletType
+    {
+        normal = 1,
+        lazer,
+        rocket
+    }
 
     public int damage;
     public float speed;
     public float life;
 
+    bulletType type;
+
     [HideInInspector]
     public string shooterTag;
 
-	void Start () {
-        damage = 10;
+    void Awake()
+    {
+        type = bulletType.normal;
+        damage = 5;
         speed = 0.4f;
         life = 3f;
-	}
+    }
+
+    public void initialize(int a_type)
+    {
+        type = (bulletType)a_type;
+
+        if (type == bulletType.normal)
+        {
+            damage = 5;
+            speed = 0.5f;
+            life = 3f;
+        }
+        else if (type == bulletType.lazer)
+        {
+            damage = 1;
+            speed = 0;
+            life = 3f;
+        }
+        else if (type == bulletType.rocket)
+        {
+            damage = 20;
+            speed = 0.2f;
+            life = 3f;
+        }
+    }
 	
-	void Update () {
-        life -= Time.deltaTime;
-        transform.position += transform.forward * speed;
-	}
+	void Update()
+    {
+        if (type == bulletType.normal)
+        {
+            life -= Time.deltaTime;
+            transform.position += transform.forward * speed;
+        }
+        else if (type == bulletType.lazer)
+        {
+
+        }
+        else if (type == bulletType.rocket)
+        {
+            life -= Time.deltaTime;
+        }
+    }
 
     void OnTriggerEnter(Collider other)
     {
-        if (!other.CompareTag(shooterTag) && !other.CompareTag(transform.tag))
+        if (type == bulletType.normal || type == bulletType.rocket)
         {
-            // TODO: Damage other object
-            Destroy(gameObject);
+            if (!other.CompareTag(shooterTag) && !other.CompareTag(transform.tag))
+            {
+                // TODO: Damage other object
+                Destroy(gameObject);
+            }
         }
     }
 }
