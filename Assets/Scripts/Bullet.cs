a9c55e309f0e885a684be8c92aae600f62ba4039
Hydraaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa <james.dasilva1111@gmail.com>
2016-08-18 13:04:10 +1000

    Added Initial Bullet
    + Added Bullet GameObject
    + Added Bullet Script

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+    public int damage;
+    public float speed;
+    public float life;
+
+    [HideInInspector]
+    public string shooterTag;
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter(Collider other)
+    {
+        if (!other.CompareTag(shooterTag))
+        {
+            // Damage hit object
+            Destroy(gameObject);
+        }
+    }
+}
