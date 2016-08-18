f5a257f1008182c09877c2ecf0115e406a29ec47
Ryu <dragonryu1205@gmail.com>
2016-08-18 15:38:34 +1000

    movement
    added movement system

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,17 +6,26 @@ using System.Collections;
 public class Movement : MonoBehaviour
 {
     //variables
-    public int accel;
-    public int speed;
+    public float accel = 10.0f;
+    public float decel = 0.5f;
+    public float hspeed = 0.0f;
+    public float vspeed = 0.0f;
+    private const int MAX = 50;
+    private Rigidbody rb;
 
-    public void myMovement()
+    void Start()
     {
-        if (Input.GetKeyDown("W"))
-        {
+        rb = GetComponent<Rigidbody>();
+    }
 
+    void FixedUpdate()
+    {
+        hspeed = Input.GetAxis("Horizontal") ;
+        vspeed = Input.GetAxis("Vertical")   ;
 
-        }
-            
+        Vector3 move = new Vector3(hspeed, vspeed, 0.0f);
+
+        rb.AddForce(move * accel,ForceMode.Acceleration);
     }
 
 
