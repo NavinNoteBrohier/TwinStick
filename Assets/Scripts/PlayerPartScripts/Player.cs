134d74a490c1e42cdff2ba5dc9ccba03d0090ea5
Nav <navin.brohier@gmail.com>
2016-08-18 13:17:03 +1000

    Changes to organisation
    ~ changes to file directory

---
+++ Assets/Scripts/PlayerPartScripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+    // variables
+    public string name;
+    public int TotalHullpoints;
+
+    public int size;
+    public int WeaponCount;
+    
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+
+
+}
