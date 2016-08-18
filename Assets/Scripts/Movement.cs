91d6b1b80624a98cd657bbb397e242d7d22caf49
Ryu <dragonryu1205@gmail.com>
2016-08-18 13:28:49 +1000

    test
    included movement script

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class Movement : MonoBehaviour
+{
+    //variables
+    public int accel;
+    public int speed;
+
+    public void myMovement()
+    {
+        if (Input.GetKeyDown("W"))
+        {
+
+
+        }
+            
+    }
+
+
+}
